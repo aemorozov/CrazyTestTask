f96852cfebd9710dfea8b1c7bf052b134848a88a
aemorozov <admin@aemorozov.com>
2023-07-17 13:31:47 +0400

    added readme

---
+++ README.md
@@ -0,0 +1,11 @@
+- Тестовое задание
+  Написать функцию sostavChisla(massivChisel: number[], chislo: number),
+  которая бы находила все возможные комбинации чисел из massivChisel,
+  сумма которых равна chislo. При этом:
+  1. massivChisel содержит, только уникальные положительные числа (> 0)
+  2. в комбинации не должно быть повторений чисел
+  3. все комбинации должны быть уникальными
+  Для проверки работоспособности функции запустить runTests()
+  @param massivChisel: number[]
+  @param chislo: number[]
+  @return Array<Array<number>>
--- test1.js
+++ test1.js
@@ -1,18 +1,3 @@
-/**
-* Написать функцию sostavChisla(massivChisel: number[], chislo: number), 
-  которая бы находила все возможные комбинации чисел из massivChisel, 
-  сумма которых равна chislo. При этом:
-  1) massivChisel содержит, только уникальные положительные числа (> 0)
-  2) в комбинации не должно быть повторений чисел
-  3) все комбинации должны быть уникальными
-  
-  Для проверки работоспособности функции запустить runTests()
-  
-  @param massivChisel: number[]
-  @param chislo: number[]
-  @return Array<Array<number>>
-*/
-
 function sostavChisla(massivChisel, chislo) {
 
     function SortForNumbersAndRevers(a, b) {
